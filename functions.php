e9185bfa135f92efe2753fb24af21fa330d4e136
Thapelo Moeti <benzitto@gmail.com>
2020-03-24 21:07:56 +0200

    AF: to return the escaped directory uri

--- functions.php
+++ functions.php
@@ -7,3 +7,12 @@
 
 define( 'MATHOMO_VERSION', '1.0.0', true );
 
+/**
+ * Get the escaped template directory URI.
+ * 
+ * @param string $append The string to append to the URI.
+ */
+function mathomo_get_directory_uri( $append = '' ) {
+    return esc_url( get_template_directory_uri() . $append );
+}
+
