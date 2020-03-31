94cea9242daaad30ede8cd07aa767e674817ad4f
Thapelo Moeti <benzitto@gmail.com>
2020-03-31 23:45:07 +0200

    PART: to show no thumb excerpt loop

---
+++ parts/loops/no-thumbnail.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * To Show main loop. Excerpts are used and no thumbnails are shown.
+ * 
+ * @package Mathomo
+ */
+
+ ?>
+
+<div id="main-content" class="main-loop">
+    <?php
+    while( have_posts() ) {
+        the_post();
+
+        get_template_part( 'parts/excerpts/nothumb-excerpt', get_post_format() );
+    }    
+
+    mathomo_print_posts_navigation();
+    ?>
+</div>
