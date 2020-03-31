89237540ad4d6268d8d14ca56b48d69f7c02948d
Thapelo Moeti <benzitto@gmail.com>
2020-03-31 21:19:47 +0200

    AF: to print site header

---
+++ inc/template-functions.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template helper functions
+ * 
+ * @package Mathomo
+ */
+
+
+if( ! function_exists( 'mathomo_print_site_header' ) ):
+    /**
+     * Prints site header
+     *
+     * @param array $args Options for printing the site header
+     */
+    function mathomo_print_site_header( array $args = array() ) {
+        $defaults = array(
+            'wrapper_id' => '',
+            'wrapper_class' => 'site-header',
+            'print_description' => false
+        );
+
+        $args = wp_parse_args( $defaults, $args );
+
+        $content = '';
+
+        if( has_custom_logo() ) {
+            $content = get_custom_logo();
+        } else {
+            // Blog name
+            $title = __( 'Home', 'mathomo' );
+            $link = HTMLER::a( get_bloginfo( 'name' ), array( 'href' => home_url(), 'title' => $title ) );
+            
+            $content = HTMLER::h1_raw( $link, array( 'class' => 'site-name' ) );
+
+            // Blog Description
+            if( $args[ 'print_description' ] ) {
+                $content .= HTMLER::div( get_bloginfo( 'description' ), array( 'class' => 'site-description' ) );
+            }
+        }
+
+        HTMLER::div_raw_e( $content, array( 'id' => $args[ 'wrapper_id' ], 'class' => $args[ 'wrapper_class' ] ) );
+    }
+endif;
