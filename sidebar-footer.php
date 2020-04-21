f1decb5ec7350782b2792700e5326f8fcf5e087f
Thapelo Moeti <benzitto@gmail.com>
2020-04-21 22:21:59 +0200

    TEMP: footer sidebar

---
+++ sidebar-footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The footer sidebar template.
+ * 
+ * @package Mathomo
+ */
+?>
+<section id="widget-area">
+    <div class="container">
+        <div class="wrap ts-ns-3">
+            <div id="col-1">
+                <?php
+                    dynamic_sidebar( 'footer-col-1' );
+                ?>
+            </div>
+            <div id="col-2">
+                <?php
+                    dynamic_sidebar( 'footer-col-2' );
+                ?>
+            </div>
+            <div id="col-3">
+                <?php
+                    dynamic_sidebar( 'footer-col-3' );
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
