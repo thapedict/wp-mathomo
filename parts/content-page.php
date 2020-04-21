b931d9a022d7a3347f63ef704a10ccef4c490335
Thapelo Moeti <benzitto@gmail.com>
2020-04-21 19:50:24 +0200

    added title

--- parts/content-page.php
+++ parts/content-page.php
@@ -8,26 +8,28 @@
 
 <article id="post-<?php the_id(); ?>" <?php post_class(); ?>>
 <?php
-do_action( 'before_page_content' );
+    do_action( 'before_page_content' );
 
-if( has_post_thumbnail() ) {
-    print '<div class="post-thumbnail">';
-        the_post_thumbnail(array( 1024, 1024 ));
-    print '</div>';
-}
+    the_title( '<h1 class="post-title">', '</h1>' );
+
+    if( has_post_thumbnail() ) {
+        print '<div class="post-thumbnail">';
+            the_post_thumbnail( array( 1024, 1024 ) );
+        print '</div>';
+    }
 ?>
 
 <div class="post-content">
-    <?php the_content(); ?>
+<?php
+    the_content();
+?>
 </div>
 
 <?php
-mathomo_print_post_pages();
+    mathomo_print_post_pages();
 
-mathomo_edit_post_link();
+    mathomo_edit_post_link();
 
-
-
-do_action( 'after_page_content' );
+    do_action( 'after_page_content' );
 ?>
 </article>
