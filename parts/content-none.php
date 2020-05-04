539f2bc13b403e78303c97549509c85da58e4be3
Thapelo Moeti <benzitto@gmail.com>
2020-05-04 21:49:53 +0200

    MOD: reformatting, reorganising

--- parts/content-none.php
+++ parts/content-none.php
@@ -8,34 +8,32 @@
 
 <article id="post-0" class="no-results nothing-found">
 <?php
-if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-    printf(
-        '<p>' . wp_kses(
-            /* translators: 1: link to WP admin new post page. */
+HTMLER::h1_e( __( 'Oops! Nothing Found', 'mathomo' ), array( 'class' => 'page-title' ) );
+
+echo '<div class="post-content">';
+
+    if ( is_home() && current_user_can( 'publish_posts' ) ) :
+        /* translators: 1: link to WP admin new post page. */
+        $get_started = sprintf( 
             __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'mathomo' ),
-            array(
-                'a' => array(
-                    'href' => array(),
-                ),
-            )
-        ) . '</p>',
-        esc_url( admin_url( 'post-new.php' ) )
-    );
+            esc_url( admin_url( 'post-new.php' ) )
+        );
 
-else :
-    HTMLER::h4_e( __( 'Oops! Nothing Found', 'mathomo' ), array( 'class' => 'page-title' ) );
-    
-    $error_msg = is_search() ?
-        // for search
-        __( 'Your search didn\'t match anything. Please try using different keywords.', 'mathomo' ):
-        // for everything else.
-        __( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'mathomo' );
-    
-    HTMLER::p_e( $error_msg );
-    
-    get_search_form();
+        HTMLER::p_kses_e( $get_started );
+    else :    
+        $error_msg = is_search() ?
+            // for search
+            __( 'Your search didn\'t match anything. Please try using different keywords.', 'mathomo' ):
+            // for everything else.
+            __( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'mathomo' );
+        
+        HTMLER::p_kses_e( $error_msg );
+        
+        get_search_form();
+    endif;
 
-endif;
+echo '</div>';
+
 ?>
-</article><!-- .page-content -->
+</article>
