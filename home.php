5a19ef30922ccca5d4642b03b830a03baf4495cb
Thapelo Moeti <benzitto@gmail.com>
2020-05-21 22:17:19 +0200

    MOD: removed page no. because WP by default doesn't show it in titles

--- home.php
+++ home.php
@@ -16,14 +16,6 @@ get_header();
         if ( $blog_page ) {
             $page = get_page( $blog_page );
 
-            if( is_paged() ) {
-                /* translators:
-                    1: post title
-                    2: page number
-                */
-                $page->post_title = sprintf( __( '%1$s / %2$s', 'mathomo' ), $page->post_title, $paged );
-            }
-
             HTMLER::h1_kses_e( $page->post_title, array('class' => 'page-title' ) );
         }
         
