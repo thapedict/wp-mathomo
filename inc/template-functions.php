02ff750438202cf5633eb56053f0dca99c4407db
Thapelo Moeti <benzitto@gmail.com>
2020-05-27 19:38:27 +0200

    MOD: print_search_form > changed wrap id to class

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -292,7 +292,7 @@ if( ! function_exists( 'mathomo_print_search_form' ) ):
      * @param bool $for_woocommerce If we should print a product search form.
      */
     function mathomo_print_search_form( $for_woocommerce = false ) {
-        echo '<div id="mobile-drawer-search">';
+        echo '<div class="search-form-wrap">';
 
         if( $for_woocommerce && mathomo_is_woocommerce_activated() ) {
             the_widget( 'WC_Widget_Product_Search' );
