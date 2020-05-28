fd235d62b0a8d40ce44bb0eb1e933070d5a4c473
Thapelo Moeti <benzitto@gmail.com>
2020-05-28 22:15:37 +0200

    MOD: added code to handle hiding and showing of searchform & minicart

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,33 @@ $(document).ready(
     function () {
         // mobile menu
         $('#tdt-mobilenav-btn').mobilenav({style:'from-left'});
+        // search button
+        $('#search-form-btn').on('click', function(e){
+            var btn = $(this),
+                wrapper = $('.header-search-form-wrap');
+
+            if(wrapper.hasClass('shown')) {
+                wrapper.removeClass('shown').hide();
+            } else {
+                wrapper.addClass('shown').show().css({
+                    top: (btn.offset().top + btn.height()) + 'px'
+                });
+            }
+        });
+        // mini cart
+        var cartWrapper =  $('.header-mini-cart-wrap');
+        $('#mini-cart-btn').on('click', function(e){
+            var btn = $(this);
+
+            if(cartWrapper.hasClass('shown')) {
+                cartWrapper.addClass('not-shown').removeClass('shown');
+            } else {
+                cartWrapper.addClass('shown').removeClass('not-shown');
+            }
+        });
+        cartWrapper.children('#close').on('click', function(e){
+            cartWrapper.addClass('not-shown').removeClass('shown');
+        });
     }
 );
 })(jQuery);
