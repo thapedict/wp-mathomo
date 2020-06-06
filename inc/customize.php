87e4b91c8b595b61ab884f39512790d38d45440e
Thapelo Moeti <benzitto@gmail.com>
2020-06-06 17:19:58 +0200

    FUNC: update_customize_defaults

--- inc/customize.php
+++ inc/customize.php
@@ -5,3 +5,21 @@
  * @package Mathomo
  */
 
+/**
+ *  Change WordPress default.
+ * 
+ * @param WP_Customize_Manager $customize The WP_Customize_Manager.
+ */
+function mathomo_update_customize_defaults( $customize ) {
+    // Change default transport mode
+    $customize->get_setting( 'blogname' )->transport = 'postMessage';
+    $customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+
+    // Remove default header_textcolor
+    $customize->remove_setting( 'header_textcolor' );
+    $customize->remove_control( 'header_textcolor' );
+
+    // Remove default color section. (we will add it back later)
+    $customize->remove_section( 'colors' );
+}
+add_action( 'customize_register', 'mathomo_update_customize_defaults' );
