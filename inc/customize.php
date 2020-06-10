3da4cc6052d7793b779dcd503b4a4c6e7a22d530
Thapelo Moeti <benzitto@gmail.com>
2020-06-10 13:20:42 +0200

    FUNC: add_theme_color_settings
    Adds sections and color controls to the customizer

--- inc/customize.php
+++ inc/customize.php
@@ -48,3 +48,83 @@ function mathomo_get_color_settings( $id ) {
 
     return apply_filters( __FUNCTION__, $id, $settings );
 }
+
+/**
+ * Adds a color section with colors to the customizer.
+ * 
+ * @TODO Find out why 'header_textcolor' is returning "Invalid value".
+ * 
+ * @param WP_Customize_Manager $customize The WP_Customize_Manager.
+ */
+function mathomo_add_theme_colors_settings( $customize ) {
+    $customize->add_panel( 'colors_panel', array(
+        'title' => __( 'Colors' ),
+        'priority' => 10
+    ) );
+
+    // Add Color sections
+    $customize->add_section( 'header_colors', array(
+        'title' => __( 'Header', 'mathomo' ),
+        'panel' => 'colors_panel'
+    ) );
+
+    // Footer Colors Section
+    $customize->add_section( 'footer_colors', array(
+        'title' => __( 'Footer', 'mathomo' ),
+        'panel' => 'colors_panel'
+    ) );
+
+    // header_textcolor
+    $customize->add_setting( 'header_text_color', mathomo_get_color_settings( 'header_text_color' ) );
+    $customize->add_control(
+        new WP_Customize_Color_Control(
+            $customize,
+            'header_text_color',
+            array(
+                'label' => __( 'Header Text Color', 'mathomo' ),
+                'settings' => 'header_text_color',
+                'section' => 'header_colors'
+                )
+        ) 
+    );
+
+    // header_backcolor
+    $customize->add_setting( 'header_backcolor', mathomo_get_color_settings( 'header_backcolor' ) );
+    $customize->add_control(
+        new WP_Customize_Color_Control(
+            $customize,
+            'header_backcolor',
+            array(
+                'label' => __( 'Header Back Color', 'mathomo' ),
+                'section' => 'header_colors'
+                )
+        ) 
+    );
+
+    // footerwidget_textcolor
+    $customize->add_setting( 'footerwidget_textcolor', mathomo_get_color_settings( 'footerwidget_textcolor' ) );
+    $customize->add_control(
+        new WP_Customize_Color_Control(
+            $customize,
+            'footerwidget_textcolor',
+            array(
+                'label' => __( 'Widgets Area Text Color', 'mathomo' ),
+                'section' => 'footer_colors'
+                )
+        ) 
+    );
+
+    // footerwidget_backcolor
+    $customize->add_setting( 'footerwidget_backcolor', mathomo_get_color_settings( 'footerwidget_backcolor' ) );
+    $customize->add_control(
+        new WP_Customize_Color_Control(
+            $customize,
+            'footerwidget_backcolor',
+            array(
+                'label' => __( 'Widgets Area Back Color', 'mathomo' ),
+                'section' => 'footer_colors'
+                )
+        ) 
+    );
+}
+add_action( 'customize_register', 'mathomo_add_theme_colors_settings' );
