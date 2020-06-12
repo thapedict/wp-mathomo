e9959e49860d38bce3b3f1cde3c10e5886596ac7
Thapelo Moeti <benzitto@gmail.com>
2020-06-12 12:48:41 +0200

    MOD: customizer.js: updating of blogname, blogdescription, copyright_text

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -3,3 +3,38 @@
  * 
  * @package Mathomo
  */
+
+;(function($){
+    // Update the site title
+    wp.customize(
+        'blogname', function ( value ) {
+            value.bind(
+                function ( newval ) {
+                    $('.site-header .site-name a').html(newval);
+                } 
+            );
+        } 
+    );
+    
+    //Update the site description
+    wp.customize(
+        'blogdescription', function ( value ) {
+            value.bind(
+                function ( newval ) {
+                    $('.site-header .site-description').html(newval);
+                } 
+            );
+        } 
+    );
+    
+    //Update copyright text
+    wp.customize(
+        'copyright_text', function ( value ) {
+            value.bind(
+                function ( newval ) {
+                    $('.theme-credits-wrap .copyright-notice').html(newval);
+                } 
+            );
+        } 
+    );
+})(jQuery);
